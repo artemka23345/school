37c717ffdd20be79ab884ec4d6fbaa498e006c27
artemka23345 <144363632+artemka23345@users.noreply.github.com>
2024-03-08 11:53:12 +0300

    add scripts.sql

--- src/main/java/ru/hogwarts/school/scripts.sql
+++ src/main/java/ru/hogwarts/school/scripts.sql
@@ -1,5 +1,5 @@
 select * from student where age>1 and age<100
 select name from student
-select * from student where name like '%O%'
+select * from student where name like '%О%'
 select * from student where age<id
 select * from student order by age
