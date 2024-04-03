894388903d0b5f43a41d7361728111fc322ed0e2
artemka23345 <144363632+artemka23345@users.noreply.github.com>
2024-04-04 02:36:55 +0300

    sql 4.3 add sql

---
+++ src/main/java/ru/hogwarts/school/sql/script442.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users
+(
+    id          INTEGER PRIMARY KEY,
+    name        VARCHAR,
+    age         INTEGER,
+    has_license BOOLEAN,
+    car_id      INTEGER REFERENCES cars (id));
+
+CREATE TABLE cars
+(
+    id    INTEGER PRIMARY KEY,
+    brand VARCHAR,
+    model VARCHAR,
+    price  INTEGER
+);
--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -1,10 +1,11 @@
-select * from student where age>1 and age<100
-select name from student
-select * from student where name like '%О%'
-select * from student where age<id
-select * from student order by age
-SELECT * FROM faculty
-SELECT * FROM student
+select * from student where age>1 and age<100;
+select name from student;
+select * from student where name like '%О%';
+select * from student where age<id;
+select * from student order by age;
+SELECT * FROM faculty;
+SELECT * FROM student;
+
 
 
 
