0cf0359c4238452e170596413da75e332179b67c
artemka23345 <144363632+artemka23345@users.noreply.github.com>
2024-04-08 12:20:25 +0300

    log4j, added log.info all services

--- src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exeption.FacultyNotFoundException;
 import ru.hogwarts.school.model.Faculty;
@@ -10,6 +11,7 @@ import ru.hogwarts.school.service.FacultyService;
 
 import java.util.*;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class FacultyServiceImpl implements FacultyService {
@@ -23,27 +25,33 @@ public class FacultyServiceImpl implements FacultyService {
     }
 
     public Faculty findFaculty(long id) {
+        log.info("Find faculty by id");
         return facultyRepository.findById(id)
                 .orElseThrow(() -> new FacultyNotFoundException());
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        log.info("Edit faculty");
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(long id) {
+        log.info("Delete faculty by id");
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> findByColor(String color) {
+        log.info("Find faculty by color");
         return facultyRepository.findByColor(color);
     }
 
     public Collection<Faculty> findByNameOrColor(String color, String name) {
+        log.info("Find faculty by name");
         return facultyRepository.getByNameIgnoreCaseOrColorIgnoreCase(color, name);
     }
 
     public Collection<Student> getAllStudentOfFaculty(long id) {
+        log.info("Find all student  of faculty by id");
         return facultyRepository.getReferenceById(id).getStudents();
     }
 }
--- src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exeption.StudentNotFoundException;
 import ru.hogwarts.school.model.Faculty;
@@ -11,6 +12,7 @@ import ru.hogwarts.school.service.StudentService;
 
 import java.util.*;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
@@ -28,33 +30,42 @@ public class StudentServiceImpl implements StudentService {
     }
 
     public Student editStudent(Student student) {
+        log.info("Edit student");
         return studentRepository.save(student);
+
     }
 
     public void deleteStudent(long id) {
+        log.info("Delete student");
         studentRepository.deleteById(id);
     }
 
     public Collection<Student> findStudentByAge(int age) {
+        log.info("Find student by age");
         return studentRepository.findByAge(age);
     }
 
     public Collection<Student> findByAgeBetween(int max, int min) {
+        log.info("Find student by age between");
         return studentRepository.findByAgeBetween(max, min);
     }
 
     public Faculty findFacultyOfStudent(long id) {
+        log.info("Find faculty of student");
         return findStudentById(id).getFaculty();
         }
 
     @Override
     public Integer getAmountOfStudents() {
+        log.info("Get amount of students");
         return studentRepository.amountOfStudents();
     }
     public Double getAverageAge () {
+        log.info("Get average age");
         return studentRepository.averageAge();
     }
     public List<Student> getLastStudents() {
+        log.info("Get last students");
         return studentRepository.getLastStudents();
     }
 
